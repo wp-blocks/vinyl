5559fbf3e2cc158a506d6149ac658efb692d6d5f
John Hooks <bitmachina@outlook.com>
2024-02-25 11:14:35 -0800

    fix: remove experimental import in save

--- src/audio/save.tsx
+++ src/audio/save.tsx
@@ -1,31 +1,23 @@
 /**
  * WordPress dependencies
  */
-import {
-	RichText,
-	useBlockProps,
-	__experimentalGetElementClassName,
-} from '@wordpress/block-editor';
+import { RichText, useBlockProps } from '@wordpress/block-editor';
 
-export default function save({ attributes }) {
+export default function Save({ attributes }) {
 	const { autoplay, caption, loop, preload, src } = attributes;
 
 	return (
 		src && (
 			<figure {...useBlockProps.save()}>
 				<audio
-					controls
+					controls="controls"
 					src={src}
 					autoPlay={autoplay}
 					loop={loop}
 					preload={preload}
 				/>
 				{!RichText.isEmpty(caption) && (
-					<RichText.Content
-						tagName="figcaption"
-						value={caption}
-						className={__experimentalGetElementClassName('caption')}
-					/>
+					<RichText.Content tagName="figcaption" value={caption} />
 				)}
 			</figure>
 		)
